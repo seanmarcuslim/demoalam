6884fe58f69e293e8559e43fb85cdf288012af21
seanmarcuslim <seanmarcuslim@gmail.com>
2026-05-26 12:00:01 +0800

    Add translation shape check

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,9 +1,11 @@
 import { enTranslations } from './translations/en'
 import { filTranslations } from './translations/fil'
 
+export type TranslationMessages = typeof enTranslations
+
 export const translations = {
   fil: filTranslations,
   en: enTranslations,
-}
+} satisfies Record<'fil' | 'en', TranslationMessages>
 
 export type Language = keyof typeof translations
